f4e07d62a1b55fb6261c999613a24af026687a00
Andrey Parkhomenko <andidonbas@gmail.com>
2026-02-05 14:40:07 +0200

    auto sync [2026-02-05 14:40:07]

--- core/__init__.py
+++
@@ -1,6 +0,0 @@
-# core/__init__.py
-from .config import Config
-from .parser import ProxyParser
-from .utils import SingBoxManager, ConnectionTester, GeoLocator
- 
-__all__ = ['Config', 'ProxyParser', 'SingBoxManager', 'ConnectionTester', 'GeoLocator']
